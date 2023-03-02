76fa55288d13afd4cdf5d8c9a769e4e3ede4b65c
tuanvayn <tuancongtuyn@gmail.com>
2023-03-02 08:29:00 +0700

    fix: call 401 via security + move prop to env

--- containerized-gateway/src/main/java/com/tanerdiler/microservice/gateway/config/ResourceServerConfig.java
+++ containerized-gateway/src/main/java/com/tanerdiler/microservice/gateway/config/ResourceServerConfig.java
@@ -1,24 +1,51 @@
 package com.tanerdiler.microservice.gateway.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(jsr250Enabled = true)
+//@EnableGlobalMethodSecurity(jsr250Enabled = true)
 public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests()
-                .antMatchers("/api/**")
-                .hasAuthority("SCOPE_api.read")
-                .and()
-                .oauth2ResourceServer()
-                .jwt();
+//        http
+//                .cors().and().csrf().disable()
+//                .authorizeHttpRequests()
+//                .antMatchers("/**")
+//                .hasAuthority("SCOPE_api.read")
+//                .anyRequest()
+//                .authenticated()
+//                .and()
+//                .oauth2ResourceServer()
+//                .jwt();
+//    }
+//}
+
+
+//public class ResourceServerConfig {
+
+//    @Bean
+//    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http.authorizeRequests(authz -> authz.antMatchers("/**")
+//                        .hasAuthority("SCOPE_api.read")
+//                        .anyRequest()
+//                        .authenticated())
+//                .oauth2ResourceServer(oauth2 -> oauth2.jwt());
+        http.csrf(csrf -> csrf.disable())
+                .authorizeRequests(auth -> auth.anyRequest().authenticated())
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .httpBasic(Customizer.withDefaults());
+
+//        return http.build();
     }
 }
-
