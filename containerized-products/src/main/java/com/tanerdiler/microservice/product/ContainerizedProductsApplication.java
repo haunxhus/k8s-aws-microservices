260f0c912ac941e2bebcc865674e5d78186ec97d
Tony <daitt@vmogroup.com>
2023-03-29 11:00:03 +0700

    Commit config kafka and redis

--- containerized-products/src/main/java/com/tanerdiler/microservice/product/ContainerizedProductsApplication.java
+++ containerized-products/src/main/java/com/tanerdiler/microservice/product/ContainerizedProductsApplication.java
@@ -1,11 +1,20 @@
 package com.tanerdiler.microservice.product;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.cache.CacheAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableCaching
+@ImportAutoConfiguration(classes = {
+		CacheAutoConfiguration.class,
+		RedisAutoConfiguration.class
+})
 public class ContainerizedProductsApplication {
 
 	public static void main(String[] args) {
