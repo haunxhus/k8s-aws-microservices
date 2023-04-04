f114a1f9e529fe37c6554b8320dcf25c89f9d132
tuanvayn <tuancongtuyn@gmail.com>
2023-04-04 14:12:08 +0700

    refactor: format code

--- jenkins/containerized_build_k8s_scripts.sh
+++ jenkins/containerized_build_k8s_scripts.sh
@@ -19,7 +19,7 @@ K8S_TOOL_NAME=microk8s
 #sudo $K8S_TOOL_NAME kubectl rollout history deployment/k8s-service-discovery
 #sudo $K8S_TOOL_NAME kubectl get deployments -n=k8s-containerized-services
 
-#undo most recent update 
+#undo most recent update
 #sudo $K8S_TOOL_NAME kubectl rollout undo deployment/k8s-service-discovery
 
 COMMIT_HASH=
@@ -42,127 +42,116 @@ if [ "$4" ]; then
   K8S_SERVICE_NAMESPACE=$4
 fi
 
-
 echo "Move file all content in  $HOME/microservice-k8s-tar/k8s-example-tar-folder/ go outside"
 if [ -d "$HOME"/microservice-k8s-tar/k8s-example-tar-folder/ ]; then
-	mv -v "$HOME"/microservice-k8s-tar/k8s-example-tar-folder/* $HOME/microservice-k8s-tar/
-	sudo rm -r "$HOME"/microservice-k8s-tar/k8s-example-tar-folder
+  mv -v "$HOME"/microservice-k8s-tar/k8s-example-tar-folder/* $HOME/microservice-k8s-tar/
+  sudo rm -r "$HOME"/microservice-k8s-tar/k8s-example-tar-folder
 fi
 
 echo "Move file all content in $HOME/microservice-k8s-tar/$COMMIT_HASH/k8s-example-tar-folder/ go outside"
 if [ -d "$HOME"/microservice-k8s-tar/$COMMIT_HASH/k8s-example-tar-folder/ ]; then
-	mv -v "$HOME"/microservice-k8s-tar/$COMMIT_HASH/k8s-example-tar-folder/* "$HOME"/microservice-k8s-tar/
-	sudo rm -r "$HOME"/microservice-k8s-tar/$COMMIT_HASH/k8s-example-tar-folder
+  mv -v "$HOME"/microservice-k8s-tar/$COMMIT_HASH/k8s-example-tar-folder/* "$HOME"/microservice-k8s-tar/
+  sudo rm -r "$HOME"/microservice-k8s-tar/$COMMIT_HASH/k8s-example-tar-folder
 fi
 
 echo "Move file all content in $HOME/microservice-k8s-tar/$COMMIT_HASH/$COMMIT_HASH/ go outside"
 if [ -d "$HOME"/microservice-k8s-tar/$COMMIT_HASH/$COMMIT_HASH/ ]; then
-	mv -v "$HOME"/microservice-k8s-tar/$COMMIT_HASH/$COMMIT_HASH/* $HOME/microservice-k8s-tar/$COMMIT_HASH/
-	sudo rm -r "$HOME"/microservice-k8s-tar/$COMMIT_HASH/$COMMIT_HASH
+  mv -v "$HOME"/microservice-k8s-tar/$COMMIT_HASH/$COMMIT_HASH/* $HOME/microservice-k8s-tar/$COMMIT_HASH/
+  sudo rm -r "$HOME"/microservice-k8s-tar/$COMMIT_HASH/$COMMIT_HASH
 fi
 
-echo "############################################### My name is $USER";
-
-
+echo "############################################### My name is $USER"
 
 declare -a CONTAINER_NAME=("containerized-accounts" "containerized-config-server" "containerized-discovery" "containerized-gateway" "containerized-logstash" "containerized-main" "containerized-orders" "containerized-products" "containerized-prometheus" "zipkin-server")
 declare -a DEPLOYMENT_NAME=("k8s-service-accounts" "k8s-service-config-server" "k8s-service-discovery" "k8s-service-gateway" "k8s-service-logstash" "k8s-service-main" "k8s-service-orders" "k8s-service-products" "k8s-service-prometheus" "k8s-service-zipkin")
 
 ######################################################### change config first ###############################
 
-for entry in "$HOME/microservice-k8s-tar/$COMMIT_HASH/config"/*
-do
-    echo "######################## $K8S_TOOL_NAME kubectl apply -f $entry ..."
+for entry in "$HOME/microservice-k8s-tar/$COMMIT_HASH/config"/*; do
+  echo "######################## $K8S_TOOL_NAME kubectl apply -f $entry ..."
 
-	for (( i=0; i<${#CONTAINER_NAME[@]}; i++ ));
-	do
-		containerN="${CONTAINER_NAME[$i]}"
-		echo "############################# config file: $entry"
-		if [[ "$entry" == *"$containerN"* ]]; then
-			deploymentName="${DEPLOYMENT_NAME[$i]}"
-			CURRENT_IMAGE_VERSION="$(sudo $K8S_TOOL_NAME kubectl get deployment $deploymentName -o=jsonpath='{.spec.template.spec.containers[*].image}' -n="$K8S_SERVICE_NAMESPACE")"
-			echo "############################# replace data s|image:\s+$containerN.*|image: $CURRENT_IMAGE_VERSION|g to $entry"
-			# replace initial image to current image name
-			sudo sed -i -E "s|(\s+)image:\s*.*$containerN.*|\1image: $CURRENT_IMAGE_VERSION|g" $entry
-			unset deploymentName;
-			unset CURRENT_IMAGE_VERSION;
-			unset containerN;
-			break;
-		fi
-		
-	done
-	
-	#for (( i=0; i<${#CONTAINER_NAME[@]}; i++ ));
-	#do
-	#	containerN="${CONTAINER_NAME[$i]}"
-	#	echo "#############################3 config file: $entry"
-	#	if [[ "$entry" == *"$containerN"* ]]; then
-	#		echo "############################# replace data s|image:\s+$containerN.*|image: $containerN$COMMIT_HASH:$IMAGE_VERSION|g to $entry"
-	#		#sudo sed -i -E "s|(\s+)image:\s*.*$containerN.*|\1image: $REGISTRY_URL/${containerN}_${COMMIT_HASH}:$IMAGE_VERSION|g" $entry
-	#		break;
-	#	fi
-	#done
-	
-	sudo $K8S_TOOL_NAME kubectl apply -f "$entry"
+  for ((i = 0; i < ${#CONTAINER_NAME[@]}; i++)); do
+    containerN="${CONTAINER_NAME[$i]}"
+    echo "############################# config file: $entry"
+    if [[ "$entry" == *"$containerN"* ]]; then
+      deploymentName="${DEPLOYMENT_NAME[$i]}"
+      CURRENT_IMAGE_VERSION="$(sudo $K8S_TOOL_NAME kubectl get deployment $deploymentName -o=jsonpath='{.spec.template.spec.containers[*].image}' -n="$K8S_SERVICE_NAMESPACE")"
+      echo "############################# replace data s|image:\s+$containerN.*|image: $CURRENT_IMAGE_VERSION|g to $entry"
+      # replace initial image to current image name
+      sudo sed -i -E "s|(\s+)image:\s*.*$containerN.*|\1image: $CURRENT_IMAGE_VERSION|g" $entry
+      unset deploymentName
+      unset CURRENT_IMAGE_VERSION
+      unset containerN
+      break
+    fi
+
+  done
+
+  #for (( i=0; i<${#CONTAINER_NAME[@]}; i++ ));
+  #do
+  #	containerN="${CONTAINER_NAME[$i]}"
+  #	echo "#############################3 config file: $entry"
+  #	if [[ "$entry" == *"$containerN"* ]]; then
+  #		echo "############################# replace data s|image:\s+$containerN.*|image: $containerN$COMMIT_HASH:$IMAGE_VERSION|g to $entry"
+  #		#sudo sed -i -E "s|(\s+)image:\s*.*$containerN.*|\1image: $REGISTRY_URL/${containerN}_${COMMIT_HASH}:$IMAGE_VERSION|g" $entry
+  #		break;
+  #	fi
+  #done
+
+  sudo $K8S_TOOL_NAME kubectl apply -f "$entry"
 done
-
 
 #################################################################change image and resource later#######################
 
-for entry in "$HOME/microservice-k8s-tar/$COMMIT_HASH/image"/*
-do
-	IFS='/' read -ra ADDR <<< "$entry"
-	FILE="${ADDR[-1]}"
-	
-	IFS='.' read -ra SPLIT <<< "$FILE"
-	
-	echo "######################## sudo docker load -i $HOME/microservice-k8s-tar/$COMMIT_HASH/image/$FILE "
-	# https://$K8S_TOOL_NAME.io/docs/registry-built-in
-	sudo docker load -i "$HOME"/microservice-k8s-tar/$COMMIT_HASH/image/"$FILE"
-	
-	echo "######################## sudo docker tag ${SPLIT[0]}:$IMAGE_VERSION $REGISTRY_URL/${SPLIT[0]}:$IMAGE_VERSION"
-	sudo docker tag "${SPLIT[0]}:$IMAGE_VERSION"  "$REGISTRY_URL/${SPLIT[0]}:$IMAGE_VERSION"
-	
-	echo "sudo docker push $REGISTRY_URL/${SPLIT[0]}:$IMAGE_VERSION"
-	sudo docker push "$REGISTRY_URL/${SPLIT[0]}:$IMAGE_VERSION"
-	
-	echo "sudo docker images $REGISTRY_URL/*"
-	sudo docker images "$REGISTRY_URL"/*
-	#sudo docker image tag "$SPLIT[0]":$IMAGE_VERSION linhpv5555/"$SPLIT[0]":$IMAGE_VERSION
-	#sudo docker image push linhpv5555/"$SPLIT[0]":$IMAGE_VERSION
-	
-	
-	echo "######################## $K8S_TOOL_NAME ctr image import $FILE ... "
-	# https://$K8S_TOOL_NAME.io/docs/registry-images
-	sudo $K8S_TOOL_NAME ctr image import $HOME/microservice-k8s-tar/$COMMIT_HASH/image/"$FILE"
-    #sudo $K8S_TOOL_NAME ctr images ls
-	
-	IFS='_' read -ra NAME <<< "${ADDR[-1]}"
-	containerName="${NAME[0]}"
-	
-	for (( i=0; i<${#CONTAINER_NAME[@]}; i++ ));
-	do
-	  if [[ "$containerName" == *"${CONTAINER_NAME[$i]}"* ]]; then
-	  
-	    echo "######################## $K8S_TOOL_NAME kubectl set image deployment/${DEPLOYMENT_NAME[$i]} ..."
-	    # kubectl set image deployment/<<deployment-name>> -n=<<namespace>> <<container_name>>=<<your_dockerhub_username>>/<<image_name you want to set now>>:<<tag_of_the_image_you_want>>
-		DEPLOYNAME="${DEPLOYMENT_NAME[$i]}"
-		CONTAINERNAME="${CONTAINER_NAME[$i]}"
-		
-		sudo $K8S_TOOL_NAME kubectl set image deployment/"$DEPLOYNAME" -n=$K8S_SERVICE_NAMESPACE $CONTAINERNAME=$REGISTRY_URL/"${SPLIT[0]}:$IMAGE_VERSION" --record
-		
-		echo "######################## $K8S_TOOL_NAME kubectl $K8S_TOOL_NAME kubectl rollout history deployment/$DEPLOYNAME..."
-		#sudo $K8S_TOOL_NAME kubectl rollout status deployment/$DEPLOYMENT_NAME[$i] -n=$K8S_SERVICE_NAMESPACE
-		sudo $K8S_TOOL_NAME kubectl rollout history deployment/"$DEPLOYNAME" -n=$K8S_SERVICE_NAMESPACE
+for entry in "$HOME/microservice-k8s-tar/$COMMIT_HASH/image"/*; do
+  IFS='/' read -ra ADDR <<<"$entry"
+  FILE="${ADDR[-1]}"
 
-		#undo most recent update 
-		#sudo $K8S_TOOL_NAME kubectl rollout undo deployment/${DEPLOYMENT_NAME[$i]}
-	  fi
-	done
-	
+  IFS='.' read -ra SPLIT <<<"$FILE"
+
+  echo "######################## sudo docker load -i $HOME/microservice-k8s-tar/$COMMIT_HASH/image/$FILE "
+  # https://$K8S_TOOL_NAME.io/docs/registry-built-in
+  sudo docker load -i "$HOME"/microservice-k8s-tar/$COMMIT_HASH/image/"$FILE"
+
+  echo "######################## sudo docker tag ${SPLIT[0]}:$IMAGE_VERSION $REGISTRY_URL/${SPLIT[0]}:$IMAGE_VERSION"
+  sudo docker tag "${SPLIT[0]}:$IMAGE_VERSION" "$REGISTRY_URL/${SPLIT[0]}:$IMAGE_VERSION"
+
+  echo "sudo docker push $REGISTRY_URL/${SPLIT[0]}:$IMAGE_VERSION"
+  sudo docker push "$REGISTRY_URL/${SPLIT[0]}:$IMAGE_VERSION"
+
+  echo "sudo docker images $REGISTRY_URL/*"
+  sudo docker images "$REGISTRY_URL"/*
+  #sudo docker image tag "$SPLIT[0]":$IMAGE_VERSION linhpv5555/"$SPLIT[0]":$IMAGE_VERSION
+  #sudo docker image push linhpv5555/"$SPLIT[0]":$IMAGE_VERSION
+
+  echo "######################## $K8S_TOOL_NAME ctr image import $FILE ... "
+  # https://$K8S_TOOL_NAME.io/docs/registry-images
+  sudo $K8S_TOOL_NAME ctr image import $HOME/microservice-k8s-tar/$COMMIT_HASH/image/"$FILE"
+  #sudo $K8S_TOOL_NAME ctr images ls
+
+  IFS='_' read -ra NAME <<<"${ADDR[-1]}"
+  containerName="${NAME[0]}"
+
+  for ((i = 0; i < ${#CONTAINER_NAME[@]}; i++)); do
+    if [[ "$containerName" == *"${CONTAINER_NAME[$i]}"* ]]; then
+
+      echo "######################## $K8S_TOOL_NAME kubectl set image deployment/${DEPLOYMENT_NAME[$i]} ..."
+      # kubectl set image deployment/<<deployment-name>> -n=<<namespace>> <<container_name>>=<<your_dockerhub_username>>/<<image_name you want to set now>>:<<tag_of_the_image_you_want>>
+      DEPLOYNAME="${DEPLOYMENT_NAME[$i]}"
+      CONTAINERNAME="${CONTAINER_NAME[$i]}"
+
+      sudo $K8S_TOOL_NAME kubectl set image deployment/"$DEPLOYNAME" -n=$K8S_SERVICE_NAMESPACE $CONTAINERNAME=$REGISTRY_URL/"${SPLIT[0]}:$IMAGE_VERSION" --record
+
+      echo "######################## $K8S_TOOL_NAME kubectl $K8S_TOOL_NAME kubectl rollout history deployment/$DEPLOYNAME..."
+      #sudo $K8S_TOOL_NAME kubectl rollout status deployment/$DEPLOYMENT_NAME[$i] -n=$K8S_SERVICE_NAMESPACE
+      sudo $K8S_TOOL_NAME kubectl rollout history deployment/"$DEPLOYNAME" -n=$K8S_SERVICE_NAMESPACE
+
+      #undo most recent update
+      #sudo $K8S_TOOL_NAME kubectl rollout undo deployment/${DEPLOYMENT_NAME[$i]}
+    fi
+  done
+
 done
-
-
 
 ########################################################################################
 
@@ -172,22 +161,21 @@ LIST_DATA="($(ls -1t -d "$HOME/microservice-k8s-tar/"*))"
 #lentghLD=$(( ${#LIST_DATA[@]} - 6))
 lentghLD=${#LIST_DATA[@]}
 
-for (( i=5; i<lentghLD; i++ ));
-  do
-    echo "DATA: ${LIST_DATA[$i]}"
-	if [[ -d "${LIST_DATA[$i]}" ]]; then
-		sudo rm -r "${LIST_DATA[$i]}"
-    fi
-	if [[ -e "${LIST_DATA[$i]}" ]]; then
-		sudo rm "${LIST_DATA[$i]}"
-    fi
+for ((i = 5; i < lentghLD; i++)); do
+  echo "DATA: ${LIST_DATA[$i]}"
+  if [[ -d "${LIST_DATA[$i]}" ]]; then
+    sudo rm -r "${LIST_DATA[$i]}"
+  fi
+  if [[ -e "${LIST_DATA[$i]}" ]]; then
+    sudo rm "${LIST_DATA[$i]}"
+  fi
 done
 
-unset COMMIT_HASH;
-unset IMAGE_VERSION;
-unset K8S_SERVICE_NAMESPACE;
-unset CONTAINER_NAME;
-unset DEPLOYMENT_NAME;
-unset LIST_DATA;
-unset lentghLD;
-unset REGISTRY_URL;
+unset COMMIT_HASH
+unset IMAGE_VERSION
+unset K8S_SERVICE_NAMESPACE
+unset CONTAINER_NAME
+unset DEPLOYMENT_NAME
+unset LIST_DATA
+unset lentghLD
+unset REGISTRY_URL
--- jenkins/ssh_run_k8s_remote_server_script.sh
+++ jenkins/ssh_run_k8s_remote_server_script.sh
@@ -12,7 +12,6 @@ if [ "$1" ]; then
   REMOTE_USER=$1
 fi
 
-
 REMOTE_IP=34.124.237.137
 if [ "$2" ]; then
   REMOTE_IP=$2
@@ -38,17 +37,16 @@ if [ "$6" ]; then
   VERSION=$6
 fi
 
-
 # automating connect to server using sshpass: sshpass  is  a  utility  designed for running ssh using the mode referred to as "keyboard-
 # interactive" password authentication, but in non-interactive mode.
 # Syntax: sshpass [-ffilename|-dnum|-ppassword|-e] [options] command arguments
 # Refer: https://manpages.ubuntu.com/manpages/trusty/man1/sshpass.1.html
 #        https://www.howtoinstall.me/ubuntu/18-04/sshpass/
 #        https://www.cyberciti.biz/faq/noninteractive-shell-script-ssh-password-provider/
-# Example: sshpass -p '$PASSWORD' ssh -o StrictHostKeyChecking=no $USERNAME@$SERVERHOST 
+# Example: sshpass -p '$PASSWORD' ssh -o StrictHostKeyChecking=no $USERNAME@$SERVERHOST
 #
 
-echo " ##################################### SSH to remote server $REMOTE_USER@$REMOTE_IP ###########################################";
+echo " ##################################### SSH to remote server $REMOTE_USER@$REMOTE_IP ###########################################"
 
 #ssh -vvv -i /home/jenkins/id_rsa vienlv@34.126.75.224
 #sudo scp /var/lib/jenkins/k8s-example-tar-folder/containerized-discovery-5eead8f264e217c7183cde558228e91e3b0684eb:v1.0.0.tar vienlv@34.126.75.224:/home/vienlv/microservice-k8s-tar/
@@ -59,13 +57,13 @@ echo " ##################################### SSH to remote server $REMOTE_USER@$
 
 # only remote to server if exist child file
 # ref: https://stackoverflow.com/questions/91368/checking-from-shell-script-if-a-directory-contains-files
-shopt -s nullglob dotglob     # To include hidden files
+shopt -s nullglob dotglob # To include hidden files
 files=("$HOME"/k8s-example-tar-folder/$CURRENT_HASH/*)
-if [ ${#files[@]} -gt 0 ]; then 
-	
-	echo "Starting SSH to $REMOTE_USER@$REMOTE_IP...."
-	
-	sudo ssh -v -o StrictHostKeyChecking=no -i "$JENKIN_SSH_KEY_PATH" "$REMOTE_USER"@$REMOTE_IP << EOSSH1
+if [ ${#files[@]} -gt 0 ]; then
+
+  echo "Starting SSH to $REMOTE_USER@$REMOTE_IP...."
+
+  sudo ssh -v -o StrictHostKeyChecking=no -i "$JENKIN_SSH_KEY_PATH" "$REMOTE_USER"@$REMOTE_IP <<EOSSH1
 	echo "current location $(pwd)"
 	echo "######### Create folder $REMOTE_HOME_PATH/microservice-k8s-tar/ if it is not exist"
 	if [[ ! -d $REMOTE_HOME_PATH/microservice-k8s-tar/ ]]; then
@@ -83,15 +81,15 @@ if [ ${#files[@]} -gt 0 ]; then
 	fi
 	
 EOSSH1
-	
-	#ref: https://linuxize.com/post/how-to-use-scp-command-to-securely-transfer-files/
-	#ref: https://linux.die.net/man/1/scp
-	set -eo pipefail
-	sudo scp -v -o StrictHostKeyChecking=no -i "$JENKIN_SSH_KEY_PATH" -r "$HOME"/k8s-example-tar-folder/ "$REMOTE_USER"@$REMOTE_IP:"$REMOTE_HOME_PATH"/microservice-k8s-tar/
-	sudo scp -v -o StrictHostKeyChecking=no -i "$JENKIN_SSH_KEY_PATH" -r "$PWD"/jenkins/containerized_build_k8s_scripts.sh "$REMOTE_USER"@$REMOTE_IP:"$REMOTE_HOME_PATH"/containerized_build_k8s_scripts.sh
 
-#ref: https://qirolab.com/posts/how-to-run-command-using-ssh-on-remote-machine-1602429142
-	sudo ssh -v -o StrictHostKeyChecking=no -i $JENKIN_SSH_KEY_PATH $REMOTE_USER@$REMOTE_IP << EOSSH
+  #ref: https://linuxize.com/post/how-to-use-scp-command-to-securely-transfer-files/
+  #ref: https://linux.die.net/man/1/scp
+  set -eo pipefail
+  sudo scp -v -o StrictHostKeyChecking=no -i "$JENKIN_SSH_KEY_PATH" -r "$HOME"/k8s-example-tar-folder/ "$REMOTE_USER"@$REMOTE_IP:"$REMOTE_HOME_PATH"/microservice-k8s-tar/
+  sudo scp -v -o StrictHostKeyChecking=no -i "$JENKIN_SSH_KEY_PATH" -r "$PWD"/jenkins/containerized_build_k8s_scripts.sh "$REMOTE_USER"@$REMOTE_IP:"$REMOTE_HOME_PATH"/containerized_build_k8s_scripts.sh
+
+  #ref: https://qirolab.com/posts/how-to-run-command-using-ssh-on-remote-machine-1602429142
+  sudo ssh -v -o StrictHostKeyChecking=no -i $JENKIN_SSH_KEY_PATH $REMOTE_USER@$REMOTE_IP <<EOSSH
 	echo ""################### current location $(pwd)"
 	echo ""################### list directory"
 	sudo ls -la
@@ -101,13 +99,13 @@ EOSSH1
 	sudo rm containerized_build_k8s_scripts.sh
 	
 EOSSH
-	#exit;
+  #exit;
 fi
 
-unset CURRENT_HASH;
-unset VERSION;
-unset REMOTE_USER;
-unset REMOTE_IP;
-unset REMOTE_HOME_PATH;
-unset JENKIN_SSH_KEY_PATH;
-unset files;
+unset CURRENT_HASH
+unset VERSION
+unset REMOTE_USER
+unset REMOTE_IP
+unset REMOTE_HOME_PATH
+unset JENKIN_SSH_KEY_PATH
+unset files
