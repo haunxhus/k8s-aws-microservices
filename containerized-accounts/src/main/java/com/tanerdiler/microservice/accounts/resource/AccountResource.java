bd7eb43f63a427e5719bedb194e2436705b96b42
haunxhus <haunx.hus@gmail.com>
2023-05-11 17:15:08 +0700

    Update AccountResource.java

--- containerized-accounts/src/main/java/com/tanerdiler/microservice/accounts/resource/AccountResource.java
+++ containerized-accounts/src/main/java/com/tanerdiler/microservice/accounts/resource/AccountResource.java
@@ -31,7 +31,7 @@ public class AccountResource {
 
     @GetMapping("/own-repository-name")
     public String getOwnRepositoryName() {
-        return "VMO TEAM 2023, demo now 4 boring so much ver 4.2";
+        return "HELLO ### VMO TEAM 2023, demo now 4 boring so much ver 4.2";
     }
 
 
